5c7f69aa2318eaed96d1db7f0daceb4a11f950fb
Mohamed ELIDRISSI <mohamed.dev43@gmail.com>
2019-11-03 17:30:10 +0100

    Make Header a functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Box, Flex, Heading, Link } from "@primer/components";
 
-export class Header extends Component {
-  render() {
-    return (
-      <Flex
-        as="header"
-        bg="#333"
-        color="#fff"
-        flexDirection="column"
-        alignItems="center"
-        py={4}
-      >
-        <Box>
-          <Heading>TodoList</Heading>
-        </Box>
-        <Box as="nav" pt={2}>
-          <Link as={RouterLink} to="/" style={LinkStyle} mr={2}>
-            Home
-          </Link>
-          |
-          <Link as={RouterLink} to="/about" style={LinkStyle} ml={2}>
-            About
-          </Link>
-        </Box>
-      </Flex>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <Flex
+      as="header"
+      bg="#333"
+      color="#fff"
+      flexDirection="column"
+      alignItems="center"
+      py={4}
+    >
+      <Box>
+        <Heading>TodoList</Heading>
+      </Box>
+      <Box as="nav" pt={2}>
+        <Link as={RouterLink} to="/" style={LinkStyle} mr={2}>
+          Home
+        </Link>
+        |
+        <Link as={RouterLink} to="/about" style={LinkStyle} ml={2}>
+          About
+        </Link>
+      </Box>
+    </Flex>
+  );
+};
 
 const LinkStyle = {
   color: "#fff",
