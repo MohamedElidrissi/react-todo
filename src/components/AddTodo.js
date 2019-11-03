2c72bd839dd1a9fe4860b52c1ded91423c6b2a62
Mohamed ELIDRISSI <mohamed.dev43@gmail.com>
2019-11-03 17:37:58 +0100

    Introduce Hooks to AddItem component

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import {
   BorderBox,
@@ -8,49 +8,39 @@ import {
   TextInput
 } from "@primer/components";
 
-export class AddTodo extends Component {
-  constructor(props) {
-    super(props);
+const AddTodo = ({ addTodo }) => {
+  const [title, setTitle] = useState("");
 
-    this.state = { todoTitle: "" };
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onSubmit(e) {
+  const onSubmit = e => {
     e.preventDefault();
-    this.props.addTodo(this.state.todoTitle);
-    this.setState({ todoTitle: "" });
-  }
+    addTodo(title);
+    setTitle("");
+  };
 
-  onChange(e) {
-    this.setState({ todoTitle: e.target.value });
-  }
+  const onChange = e => setTitle(e.target.value);
 
-  render() {
-    return (
-      <Grid
-        gridTemplateColumns={[
-          "auto 80% auto",
-          "auto 70% auto",
-          "auto 60% auto",
-          "auto 50% auto"
-        ]}
-      >
-        <BorderBox my={4} p={4} style={{ gridColumn: "2/3" }}>
-          <Flex as="form" flexDirection="column" onSubmit={this.onSubmit}>
-            <TextInput
-              placeholder="e.g. Learn React"
-              value={this.state.todoTitle}
-              onChange={this.onChange}
-            />
-            <ButtonPrimary mt={2}>Add Todo</ButtonPrimary>
-          </Flex>
-        </BorderBox>
-      </Grid>
-    );
-  }
-}
+  return (
+    <Grid
+      gridTemplateColumns={[
+        "auto 80% auto",
+        "auto 70% auto",
+        "auto 60% auto",
+        "auto 50% auto"
+      ]}
+    >
+      <BorderBox my={4} p={4} style={{ gridColumn: "2/3" }}>
+        <Flex as="form" flexDirection="column" onSubmit={onSubmit}>
+          <TextInput
+            placeholder="e.g. Learn React"
+            value={title}
+            onChange={onChange}
+          />
+          <ButtonPrimary mt={2}>Add Todo</ButtonPrimary>
+        </Flex>
+      </BorderBox>
+    </Grid>
+  );
+};
 
 AddTodo.propTypes = {
   addTodo: PropTypes.func.isRequired
