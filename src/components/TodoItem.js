ef17911c7fd5ab52ca8d090ea4a9ab4ce933c00b
Mohamed ELIDRISSI <mohamed.dev43@gmail.com>
2019-11-03 18:03:44 +0100

    Introduce Hooks to TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,34 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { ButtonDanger, Flex, Text } from "@primer/components";
 
-export class TodoItem extends Component {
-  TitleStyle = () => {
+const TodoItem = ({ todo, toggleComplete, deleteTodo }) => {
+  const TitleStyle = () => {
     return {
       flex: "auto",
-      textDecoration: this.props.todo.completed ? "line-through" : "none"
+      textDecoration: todo.completed ? "line-through" : "none"
     };
   };
 
-  render() {
-    const { todo } = this.props;
-    return (
-      <Flex alignItems="center">
-        <input
-          type="checkbox"
-          checked={this.props.todo.completed}
-          onChange={this.props.toggleComplete.bind(this, todo.id)}
-        ></input>
-        <Text as="p" ml={2} style={this.TitleStyle()}>
-          {todo.title}
-        </Text>
-        <ButtonDanger onClick={this.props.deleteTodo.bind(null, todo.id)}>
-          x
-        </ButtonDanger>
-      </Flex>
-    );
-  }
-}
+  return (
+    <Flex alignItems="center">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={toggleComplete.bind(this, todo.id)}
+      ></input>
+      <Text as="p" ml={2} style={TitleStyle()}>
+        {todo.title}
+      </Text>
+      <ButtonDanger onClick={deleteTodo.bind(null, todo.id)}>x</ButtonDanger>
+    </Flex>
+  );
+};
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
